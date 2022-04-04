067277fbea3148b9d35a7d50ba1e4d80a4fcb9ad
Maia <mmaia.inf@gmail.com>
2022-04-04 15:12:39 -0300

    Fix

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,22 +1,11 @@
 const mongoose = require("mongoose");
+const uri = process.env.MONGODB_URI;
 
-const USER_DB = "admin";
-const PASS_DB = "jean2004";
-const dbConfig =
-  "mongodb+srv://admin:jean2004@dbcurriculo.borwy.mongodb.net/dbcurriculo?retryWrites=true&w=majority";
 
-// https://data.mongodb-api.com/app/data-mbqyl/endpoint/data/beta
 
-const connection = mongoose
-  .connect(
-    `mongodb+srv://${USER_DB}:${PASS_DB}@dbcurriculo.borwy.mongodb.net/dbcurriculo?retryWrites=true&w=majority`,
-    // {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // }
-  )
+const connection = mongoose.connect(uri)
   .then(() => {
-    console.log("Conectado");    
+    console.log("Conectado");
   })
   .catch((err) => {
     console.log(err);
