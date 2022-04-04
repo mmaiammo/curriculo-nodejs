731b611186f50bc301651bd06b1846c484387589
Maia <mmaia.inf@gmail.com>
2022-04-04 15:09:49 -0300

    Fix Connection String MonoDB

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 
-const dbConfig = 'mongodb+srv://admin:jean2004@dbcurriculo.borwy.mongodb.net/dbcurriculo?retryWrites=true&w=majority';
+const USER_DB = "admin";
+const PASS_DB = "jean2004";
+const dbConfig =
+  "mongodb+srv://admin:jean2004@dbcurriculo.borwy.mongodb.net/dbcurriculo?retryWrites=true&w=majority";
 
-const connection = mongoose.connect(dbConfig, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// https://data.mongodb-api.com/app/data-mbqyl/endpoint/data/beta
+
+const connection = mongoose
+  .connect(
+    `mongodb+srv://${USER_DB}:${PASS_DB}@dbcurriculo.borwy.mongodb.net/dbcurriculo?retryWrites=true&w=majority`,
+    // {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
+  )
+  .then(() => {
+    console.log("Conectado");    
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 module.exports = connection;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const routes = require('./routes'); 
 const port = process.env.PORT || 3333;
-
 const app = express();
 require('./config/dbConfig');
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
-    console.info("Rodando");
-});
+app.listen(port);
