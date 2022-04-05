39305a8efa0eaaa3512a66f3a39fdf45e1dafd81
Maia <mmaia.inf@gmail.com>
2022-04-04 22:57:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require('./routes'); 
 const port = process.env.PORT || 3333;
 console.log("port:" + port);
+console.log("port:" + process.env.SECRET_KEY);
 const app = express();
 require('./config/dbConfig');
 
