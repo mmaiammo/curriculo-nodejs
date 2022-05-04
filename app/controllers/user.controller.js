a56e5d08c8f600c02cd188b3c66746e00af6fbe8
Maia <mmaia.inf@gmail.com>
2022-05-04 20:55:56 -0300

    Ajuste de mensagens

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,12 +1,12 @@
 exports.allAccess = (req, res) => {
-  res.status(200).send("Public Content.");
+  res.status(200).send("Conteúdo Público.");
 };
 exports.userBoard = (req, res) => {
-  res.status(200).send("User Content.");
+  res.status(200).send("Conteúdo de Usuário.");
 };
 exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+  res.status(200).send("Conteúdo de Admin.");
 };
 exports.moderatorBoard = (req, res) => {
-  res.status(200).send("Moderator Content.");
+  res.status(200).send("Conteúdo de Moderador.");
 };
